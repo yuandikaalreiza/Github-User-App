522663692e70b1b44b76a971dddb5266111a0cf7
Yuandika Alfahreiza <yuandika11@gmail.com>
2021-12-05 16:09:48 +0700

    refactor: change findViewById to viewBinding

--- app/src/main/java/com/dicoding/githubuserapp/ListGithubUserAdapter.kt
+++ app/src/main/java/com/dicoding/githubuserapp/ListGithubUserAdapter.kt
@@ -1,31 +1,24 @@
 package com.dicoding.githubuserapp
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.dicoding.githubuserapp.databinding.ItemRowUserBinding
 
 class ListGithubUserAdapter(private val listGithubUser: ArrayList<GithubUser>): RecyclerView.Adapter<ListGithubUserAdapter.ListViewHolder>() {
-    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
-        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
-        var tvUsername: TextView = itemView.findViewById(R.id.tv_item_username)
-        var tvLocation: TextView = itemView.findViewById(R.id.tv_item_location)
-    }
+    class ListViewHolder(var binding: ItemRowUserBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_user, parent, false)
-        return ListViewHolder(view)
+        val binding = ItemRowUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, username, location, photo) = listGithubUser[position]
-        holder.imgPhoto.setImageResource(photo)
-        holder.tvName.text = name
-        holder.tvUsername.text = username
-        holder.tvLocation.text = location
+        holder.binding.imgItemPhoto.setImageResource(photo)
+        holder.binding.tvItemName.text = name
+        holder.binding.tvItemUsername.text = username
+        holder.binding.tvItemLocation.text = location
     }
 
     override fun getItemCount(): Int = listGithubUser.size
